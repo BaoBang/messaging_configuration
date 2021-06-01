3648941a24141d9ead2b0ecbb8d653f3693cb0a9
bangnb <baobangb5@gmail.com>
2021-06-01 16:14:22 +0700

    FIx

--- lib/messaging_config.dart
+++ lib/messaging_config.dart
@@ -67,6 +67,8 @@ class MessagingConfig {
       setHandler();
     } else {
       FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print(jsonEncode(message));
+        print("onMessage: $message");
         inAppMessageHandlerRemoteMessage(message);
       });
       // FirebaseMessaging.onBackgroundMessage((RemoteMessage message) {
@@ -74,9 +76,13 @@ class MessagingConfig {
       //   return myBackgroundMessageHandler(message.data);
       // });
       FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage message){
+        print(jsonEncode(message));
+        print("getInitialMessage: $message");
         myBackgroundMessageHandler(message.data);
       });
       FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+        print(jsonEncode(message));
+        print("onResume: $message");
         myBackgroundMessageHandler(message.data);
       });
     }
